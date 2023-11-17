48ab0d4c65dcd9225bc661e6c3f5b5f623412aea
Jg_747 <50830651+Jg747@users.noreply.github.com>
2023-11-17 10:32:27 +0100

    Delete bin directory

--- bin/main.o.d
+++
@@ -1,4 +0,0 @@
-bin/main.o: src/main.cpp include/cparse/shunting-yard.h \
-  include/cparse/./packToken.h include/cparse/./containers.h \
-  include/cparse/./functions.h include/cparse/shunting-yard-exceptions.h \
-  include/cparse/./shunting-yard.h
